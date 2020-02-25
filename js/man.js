71f9f0e321c44fdec2a4de479d86419d135acbea
vinicius ferreira <viniciusferreira99762385@gmail.com>
2020-02-25 20:56:48 -0300

    primeira projeto js

---
+++ js/man.js
@@ -0,0 +1,56 @@
+
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";
+
+
+    //alert("Obrigado por clicar");
+}
+
+function trocar(){
+    document.getElementById("mousemove").innerHTML="Obrigado por passar o mouse";
+}
+
+function voltar(){
+    document.getElementById("mousemove").innerHTML="Passe o mouse aqui";
+}
+
+
+/*
+var d = new Date();
+alert(d.getDate());
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+*/
+
+/*
+var count = 0;
+while (count <= 5){
+    console.log (count);
+    alert(count);
+    count ++;
+};
+*/
+
+
+
+
+/*
+var idade = 18;
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};
+*/
+
+
+
+
+
+//var lista = ["maçã", "pera", "laranja"];
+//lista.push("uva");
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
